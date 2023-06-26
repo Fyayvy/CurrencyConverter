5e752091e9e1417a49b0ff5bccbd29382903e921
Fyayvy <favourogunmola33@gmail.com>
2023-06-26 14:21:27 +0100

    'ft'

---
+++ App.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from currency_converter import CurrencyConverter
+import pycountry
+
+
+st.title("Currency Converter")
+
+
+converter = CurrencyConverter()
+
+
+currencies = converter.currencies
+
+
+currency_to_country = {currency: pycountry.currencies.get(alpha_3=currency).name for currency in currencies if pycountry.currencies.get(alpha_3=currency)}
+
+
+amount = st.slider("Enter amount:",0,20000,5000)
+from_currency = st.selectbox("From_currency:",options=list(currency_to_country.keys()))
+
+from_country = currency_to_country.get(from_currency,"Unknown")
+
+
+st.write(f"From country: {from_country}")
+
+
+to_currency = st.selectbox("To_currency:",options=list(currency_to_country.keys()))
+
+to_country = currency_to_country.get(to_currency,"Unknown")
+
+
+st.write(f"To country: {to_country}")
+
+if st.button('Convert Currency'):
+	converted_amount = converter.convert(amount, from_currency, to_currency)
+	st.write(f"Converted amount: {converted_amount:.2f} {to_currency}")
+	
+
+
+
